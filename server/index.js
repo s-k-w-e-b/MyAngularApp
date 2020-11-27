45a72acc299785919bef91bd7cad90aba336c4d0
s-k-w-e-b <s.k.w.e.b.0117@gmail.com>
2020-11-27 18:41:52 +0900

    Nodeサーバー構築

---
+++ server/index.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const PORT = process.env.PORT || '3001';
+const mongoose = require('mongoose');
+const config = require('./config/dev');
+const FakeDb = require('./fake-db');
+const productRoutes = require('./routes/products');
+
+mongoose.connect(config.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  const fakeDb = new FakeDb();
+  fakeDb.initDb();
+});
+
+const app = express();
+
+app.use('/api/v1/products', productRoutes);
+
+app.get('/products', function (req, res) {
+  res.json({ 'success': true });
+});
+
+app.listen(PORT, function () {
+  console.log('I am express');
+})
